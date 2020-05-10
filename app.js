f0ce395b922185f5736879190f827954a38d0121
Mel <mel@mels-mbp.mynet>
2020-05-10 20:25:14 +0100

    removed animations from mobile

--- app.js
+++ app.js
@@ -2,65 +2,81 @@ let controller;
 let slideScene;
 let pageScene;
 
-function animateSlides() {
-  //Init controller
-  controller = new ScrollMagic.Controller();
-  // Select some things
-  const sliders = document.querySelectorAll(".slide");
-  const nav = document.querySelector(".nav-header");
-  //loop over each slide
-  sliders.forEach((slide, index, slides) => {
-    const revealImg = slide.querySelector(".reveal-img");
-    const img = slide.querySelector("img");
-    const revealText = slide.querySelector(".reveal-text");
-    //gsap
-    const slideTl = gsap.timeline({
-      defaults: { duration: 1, ease: "power2.inOut" },
-    });
-    slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
+var x = window.matchMedia("(min-width: 768px)");
 
-    //reveals the text
-    slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=1");
-    slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
-    //Create a scene
-    slideScene = new ScrollMagic.Scene({
-      triggerElement: slide,
-      triggerHook: 0.25,
-      reverse: false,
-    })
-      //starts the slide scene on the triggers
-      .setTween(slideTl)
-      //adds indicators
-      // .addIndicators({
-      //   name: "slide",
-      // })
-      .addTo(controller);
-    //second slide animation
-    const pageTl = gsap.timeline();
-    //gets next slide
-    let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
-    // pushes the next slide down
-    pageTl.fromTo(nextSlide, { y: "0%" }, { y: "50%" });
-    pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
-    //pulls the slide back up when its in frame
-    pageTl.fromTo(nextSlide, { y: "50%" }, { y: "0%" });
-    //create new scene
-    pageScene = new ScrollMagic.Scene({
-      triggerElement: slide,
-      duration: "100%",
-      triggerHook: 0,
-    })
-      // .addIndicators({
-      //   colorStart: "white",
-      //   colorTrigger: "white",
-      //   name: "page",
-      //   indent: 200,
-      // })
-      //stays on the page for 100%, push followers get rid of huge gap
-      .setPin(slide, { pushFollowers: false })
-      .setTween(pageTl)
-      .addTo(controller);
-  });
+function animateSlides() {
+  if (x.matches) {
+    //Init controller
+    controller = new ScrollMagic.Controller();
+    // Select some things
+    const sliders = document.querySelectorAll(".slide");
+    const nav = document.querySelector(".nav-header");
+    //loop over each slide
+    sliders.forEach((slide, index, slides) => {
+      const revealImg = slide.querySelector(".reveal-img");
+      const img = slide.querySelector("img");
+      const revealText = slide.querySelector(".reveal-text");
+      //gsap
+      const slideTl = gsap.timeline({
+        defaults: { duration: 1, ease: "power2.inOut" },
+      });
+      slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
+
+      //reveals the text
+      slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=1");
+      //slides nav down
+
+      slideTl.fromTo(nav, { y: "0%" }, { y: "0%" });
+
+      slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
+
+      //Create a scene
+      slideScene = new ScrollMagic.Scene({
+        triggerElement: slide,
+        triggerHook: 0.25,
+        reverse: false,
+      })
+        //starts the slide scene on the triggers
+        .setTween(slideTl)
+        //adds indicators
+        // .addIndicators({
+        //   name: "slide",
+        // })
+        .addTo(controller);
+      //second slide animation
+      const pageTl = gsap.timeline();
+      //gets next slide
+      let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
+      // pushes the next slide down
+
+      pageTl.fromTo(nextSlide, { y: "0%" }, { y: "50%" });
+      pageTl.fromTo(
+        slide,
+        { opacity: 1, scale: 1 },
+        { opacity: 0, scale: 0.5 }
+      );
+      //pulls the slide back up when its in frame
+      pageTl.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, "-=0.5");
+
+      //create new scene
+      pageScene = new ScrollMagic.Scene({
+        triggerElement: slide,
+        duration: "100%",
+        triggerHook: 0,
+      })
+        // .addIndicators({
+        //   colorStart: "white",
+        //   colorTrigger: "white",
+        //   name: "page",
+        //   indent: 200,
+        // })
+        //stays on the page for 100%, push followers get rid of huge gap
+        .setPin(slide, { pushFollowers: false })
+        .setTween(pageTl)
+        .addTo(controller);
+    });
+  } else {
+  }
 }
 
 const mouse = document.querySelector(".cursor");
